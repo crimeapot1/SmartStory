bee988f507e106e42697707f59ff6c4e609ad8a6
crimeapot1 <crimeapot@gmail.com>
2019-12-20 11:12:40 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartStory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
