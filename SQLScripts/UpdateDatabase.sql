1e4e1c143560307179f00169231be09090c92eda
crimeapot1 <crimeapot@gmail.com>
2019-12-20 13:01:29 +0300

    Добавление пустой базы данных SmartStory

---
+++ SQLScripts/UpdateDatabase.sql
@@ -0,0 +1,12 @@
+﻿USE SmartStory
+GO
+
+-- Создаем таблицу контроля версий
+BEGIN TRANSACTION
+    IF( OBJECT_ID(N'UpdateDatabaseVersions') IS NULL)
+        BEGIN
+           CREATE TABLE UpdateDatabaseVersions (VersionNumber INT NOT NULL);
+           INSERT INTO UpdateDatabaseVersions (VersionNumber) VALUES(1);
+        END
+COMMIT
+GO
